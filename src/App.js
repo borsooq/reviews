9ae71f7a29a3d1b6f95973b779456fe8f710be20
Mirko Boruc <mkbu@demant.com>
2021-11-07 15:11:52 +0100

    ready project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Review from "./Review";
+
+function App() {
+  return (
+    <main>
+      <div className="title">
+        <h2>out reviews</h2>
+        <div className="underline"></div>
+      </div>
+      <Review />
+    </main>
+  );
+}
+
+export default App;
